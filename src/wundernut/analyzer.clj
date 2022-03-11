62457a1eb942a0780cc2ad9c788cafc7d0cd4c93
Timo Friman <tjf@iki.fi>
2022-03-11 17:04:54 +0200

    Better naming

--- src/wundernut/analyzer.clj
+++ src/wundernut/analyzer.clj
@@ -75,63 +75,67 @@
 (defn morse->text
   "Parse input file path wav file to text string"
   [input-wav-path]
-  (let [input-sound                  (read-sound input-wav-path)
-        message-chunks               (chunks input-sound hz)
-        amplitudes                   (mapcat first message-chunks) ;; get mono channels to one seq
-        highs                        (->> (partition (/ hz 100) amplitudes) ;; downsample to 10 ms partitions
-                                          (map #(apply max %))  ;; find max within partition
-                                          (->highs amplitudethreshold)) ;; get up&down "timestamps"
-        partial-diffs                (map (fn [[a b]] (- b a)) (partition 2 highs)) ;; durations of dits and dashes
-        partial-diffs-between        (map (fn [[a b]] (- b a)) (partition 2 (rest highs))) ;; durations between dits and dashes
-        [dashthreshold spacethreshold] (->thresholds partial-diffs-between) ;; calculate thresholds
-        morse                        (map (fn [i] (if (> dashthreshold i) "." "-")) partial-diffs)
-        word-splits                  (map (fn [i] (cond (> i spacethreshold) " SPACE " (> dashthreshold i) "" :else " ")) partial-diffs-between)
-        morse-string                 (->> (concat word-splits [""])
-                                          (interleave morse) ;; interleave morse dits and dashes with word separators and character separators
-                                          (apply str))
-        decoded                      (->> (clojure.string/split morse-string #" ")
-                                          (map #(get morse-codes %) )
-                                          (apply str))]
+  (let [input-sound                    (read-sound input-wav-path)
+        message-chunks                 (chunks input-sound hz)
+        amplitudes                     (mapcat first message-chunks)                         ;; get mono channels to one seq
+        hilos                          (->> (partition (/ hz 100) amplitudes)                ;; downsample to 10 ms partitions
+                                            (map #(apply max %))                             ;; find max within partition
+                                            (->highs amplitudethreshold))                    ;; get up&down "timestamps"
+        durations                      (map (fn [[a b]] (- b a)) (partition 2 hilos))        ;; durations of dits and dashes
+        durations-between              (map (fn [[a b]] (- b a)) (partition 2 (rest hilos))) ;; durations between dits and dashes
+        [dashthreshold spacethreshold] (->thresholds durations-between)                      ;; calculate thresholds
+        morse                          (map (fn [i] (if (> dashthreshold i) "." "-")) durations)
+        word-splits                    (map (fn [i] (cond
+                                                      (> i spacethreshold) " SPACE "
+                                                      (> dashthreshold i) ""
+                                                      :else " ")) durations-between)
+        morse-string                   (->> (concat word-splits [""])
+                                            (interleave morse)                               ;; interleave morse dits and dashes with word separators and character separators
+                                            (apply str))
+        decoded                        (->> (clojure.string/split morse-string #" ")
+                                            (map #(get morse-codes %) )
+                                            (apply str))]
     decoded))
 
 (defn -main [& args]
   (println (morse->text (first args))))
 
-(def morse-codes {".-.-.-" \.,
-                  "-----"  \0,
-                  ".----"  \1,
-                  "..---"  \2,
-                  "...--"  \3,
-                  "....-"  \4,
-                  "....."  \5,
-                  "-...."  \6,
-                  "--..."  \7,
-                  "---.."  \8,
-                  "----."  \9,
-                  ".-"     \A,
-                  "-..."   \B,
-                  "-.-."   \C,
-                  "-.."    \D,
-                  "."      \E,
-                  "..-."   \F,
-                  "--."    \G,
-                  "...."   \H,
-                  ".."     \I,
-                  ".---"   \J,
-                  "-.-"    \K,
-                  ".-.."   \L,
-                  "--"     \M,
-                  "-."     \N,
-                  "---"    \O,
-                  ".--."   \P,
-                  "--.-"   \Q,
-                  ".-."    \R,
-                  "..."    \S,
-                  "-"      \T,
-                  "..-"    \U,
-                  "...-"   \V,
-                  ".--"    \W,
-                  "-..-"   \X,
-                  "-.--"   \Y,
+(def morse-codes {".-.-.-" \.
+                  "-----"  \0
+                  ".----"  \1
+                  "..---"  \2
+                  "...--"  \3
+                  "....-"  \4
+                  "....."  \5
+                  "-...."  \6
+                  "--..."  \7
+                  "---.."  \8
+                  "----."  \9
+                  ".-"     \A
+                  "-..."   \B
+                  "-.-."   \C
+                  "-.."    \D
+                  "."      \E
+                  "..-."   \F
+                  "--."    \G
+                  "...."   \H
+                  ".."     \I
+                  ".---"   \J
+                  "-.-"    \K
+                  ".-.."   \L
+                  "--"     \M
+                  "-."     \N
+                  "---"    \O
+                  ".--."   \P
+                  "--.-"   \Q
+                  ".-."    \R
+                  "..."    \S
+                  "-"      \T
+                  "..-"    \U
+                  "...-"   \V
+                  ".--"    \W
+                  "-..-"   \X
+                  "-.--"   \Y
                   "--.."   \Z
-                  "SPACE" \space})
+                  "SPACE"  \space ;; not part of Morse code but used as word separator.
+                  })
